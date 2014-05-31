6c48f2e2e5b7887306b0fa73aa40cf1d80ae4666
Daybrush <itoolsg@gmail.com>
2014-05-31 14:33:14 +0900

    파일 이동

--- daylight.ui.js
+++ daylight.ui.js
@@ -1,9 +1,20 @@
 daylight.UI = {};
-
-daylight.UI.drag = function(element) {
+daylight.UI.drag = {};
+daylight.UI.drag.dragstart = function(e) {
+	var dlDragTarget = $(".day-drag").has(e.dragElement, true);
+	e.dragInfo.otop = parseFloat(dlDragTarget.css("top")) || 0;
+	e.dragInfo.oleft = parseFloat(dlDragTarget.css("left")) || 0;
+}
+daylight.UI.drag.drag = function(e) {
+	var dlDragTarget = $(".day-drag").has(e.dragElement, true);
+	if(dlDragTarget.size() === 0)
+		return;
+	dlDragTarget.css("left", e.dragInfo.oleft + e.dragX);
+	dlDragTarget.css("top", e.dragInfo.otop + e.dragY);
 	
 }
-
+daylight.UI.drag.dragend = function(e) {
+}
 $(document).ready(function() {
 	$("body").drag();
 	
@@ -13,33 +24,19 @@ $(document).ready(function() {
 	
 		e.stopPropagation();
 		
-		
 		var dlElement = $(e.dragElement);
-		if(!dlElement.hasClass("day-drag-draggable"))
-			return;
-		
-
-		var dlDragTarget = $(".day-drag").has(e.dragElement, true);
-		otop = parseFloat(dlDragTarget.css("top")) || 0;
-		oleft = parseFloat(dlDragTarget.css("left")) || 0;
-		
+		if(dlElement.hasClass("day-drag-draggable"))
+			daylight.UI.drag.dragstart(e);
 
 	});
 	$("body").on("drag", function(e) {
 		e.stopPropagation();
+		
 		var dlElement = $(e.dragElement);
-		if(!dlElement.hasClass("day-drag-draggable"))
-			return;
-			
-		
-		var dlDragTarget = $(".day-drag").has(e.dragElement, true);
-		if(dlDragTarget.size() === 0)
-			return;
+		if(dlElement.hasClass("day-drag-draggable"))
+			daylight.UI.drag.drag(e);
 		
 		
-		
-		dlDragTarget.css("left", oleft + e.dragX);
-		dlDragTarget.css("top", otop + e.dragY);
 	});
 
 });
